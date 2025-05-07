74737167a1f9d19ffd968695c310da7af49d1a39
SamWebster13 <150615414+SamWebster13@users.noreply.github.com>
2025-05-06 19:46:13 -0700

    all i got so far
    no idea if this is better than previous

--- Assets/Scripts/AI/BehaviorTree/Selector.cs
+++ Assets/Scripts/AI/BehaviorTree/Selector.cs
@@ -4,6 +4,24 @@ public class Selector : InteriorNode
 {
     public override Result Run()
     {
+        while (current_child < children.Count)
+        {
+            Result res = children[current_child].Run();
+            if (res == Result.SUCCESS)
+            {
+                current_child = 0;
+                return Result.SUCCESS;        }
+            else if (res == Result.IN_PROGRESS)
+            {
+                return Result.IN_PROGRESS;
+            }
+            else if (res == Result.FAILURE)
+            {
+                current_child++;
+            }
+        }
+
+        current_child = 0;
         return Result.FAILURE;
     }
 
